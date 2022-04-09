94a470126700c38de532952ac7c6425d1a4a1e14
maisen35 <suzukixoxb5021@gmail.com>
2022-04-09 14:25:57 +0000

    [second]

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,23 +17,19 @@ class User < ApplicationRecord
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: {maximum: 50}
 
-
-
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
-  
+
   def follow(user_id)
     relationships.create(followed_id: user_id)
   end
-  
+
   def unfollow(user_id)
     relationships.find_by(followed: user_id).destroy
   end
-  
+
   def following?(user)
-    followings.includes?(user)
+    followings.include?(user)
   end
-
-
 end
